5f1416f38652212eef4bfd502f461144349f0f67
asharlaimov <sgnllsa@gmail.com>
2014-04-01 20:53:02 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebCalc::Application.config.secret_key_base = 'aaeacb51a6686f1318a70e5691cb392df3f661e895fb2e9bea86a2657b4bc54718e4cad4150f8c0b86543f1db54aebf8451c38a60ea4350a87b647c343e2b3b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebCalc::Application.config.session_store :cookie_store, key: '_WebCalc_session'
