7f2789a10b9eb1c807d229c009b7539184926ec8
Aleksey <asharlaimov@gmail.com>
2014-05-05 23:17:26 +0300

    update Calc logic

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -3,6 +3,23 @@ class CalcController < ApplicationController
   end
 
   def calculate
-    @sum = eval(params[:expression])
+    case params[:action_operator]
+      when '+'
+        @sum = params[:expr1].to_s.to_f + params[:expr2].to_s.to_f
+      when '-'
+        @sum = params[:expr1].to_s.to_f - params[:expr2].to_s.to_f
+      when '/'
+        @sum = params[:expr1].to_s.to_f / params[:expr2].to_s.to_f
+      when '√'
+        @sum = Math.sqrt(params[:expr1].to_s.to_f)
+      when '∛'
+        @sum = Math.cbrt(params[:expr1].to_s.to_f)
+      when 'sin'
+        @sum = Math.sin(params[:expr1].to_s.to_f)
+      when 'cos'
+        @sum = Math.cos(params[:expr1].to_s.to_f)
+      else
+        @sum = eval(params[:expr1])
+    end
   end
 end
