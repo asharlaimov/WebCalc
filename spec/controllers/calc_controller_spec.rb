ec057e7efd9dd551496255d0415a6b57d126b4e0
asharlaimov <sgnllsa@gmail.com>
2014-05-06 17:52:56 +0300

    update

--- spec/controllers/calc_controller_spec.rb
+++ spec/controllers/calc_controller_spec.rb
@@ -11,11 +11,44 @@ describe CalcController, :type => :controller do
 
   describe "POST 'calculate'" do
     it "returns http success" do
-      post :calculate, :expr1 => '1', :expr2 => '2', :action_operator => '+', :remote => true, :format => 'js'
+      post :calculate, :remote => true, :format => 'js'
       response.should be_success
-      assigns(:result).should == 3
       expect(response).to render_template("calculate")
     end
-  end
 
+    it "returns 1 + 2" do
+      post :calculate, :expr1 => '1', :expr2 => '2', :action_operator => '+', :remote => true, :format => 'js'
+      assigns(:result).should == 3
+    end
+
+    it "returns 1 - 2" do
+      post :calculate, :expr1 => '1', :expr2 => '2', :action_operator => '-', :remote => true, :format => 'js'
+      assigns(:result).should == -1
+    end
+
+    it "returns 1 / 2" do
+      post :calculate, :expr1 => '1', :expr2 => '2', :action_operator => '/', :remote => true, :format => 'js'
+      assigns(:result).should == 0.5
+    end
+
+    it "returns √100" do
+      post :calculate, :expr1 => '100', :action_operator => '√', :remote => true, :format => 'js'
+      assigns(:result).should == 10
+    end
+
+    it "returns ∛27" do
+      post :calculate, :expr1 => '27', :action_operator => '∛', :remote => true, :format => 'js'
+      assigns(:result).should == 3
+    end
+
+    it "returns sin 1" do
+      post :calculate, :expr1 => '1', :action_operator => 'sin', :remote => true, :format => 'js'
+      assigns(:result).should == 0.8414709848078965
+    end
+
+    it "returns cos 1" do
+      post :calculate, :expr1 => '1', :action_operator => 'cos', :remote => true, :format => 'js'
+      assigns(:result).should == 0.5403023058681398
+    end
+  end
 end
